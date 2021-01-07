c9f4b58952253b7908a81a068eba2267de00bdd3
hrmtsasan <61978130+hrmtsasan@users.noreply.github.com>
2021-01-07 15:06:27 +0530

    Update utils.py

--- Cats Dogs Horses Humans/utils/utils.py
+++ Cats Dogs Horses Humans/utils/utils.py
@@ -11,16 +11,4 @@ def decodeImage(imgstring, fileName):
 def encodeImageIntoBase64(croppedImagePath):
     with open(croppedImagePath, "rb") as f:
         return base64.b64encode(f.read())
-    # base64 is going to use lesser space as compared to total images bcoz it is images which is available in string format.
-    # This is called encoded format.
-
-    # So, we have decoding code as well to convert this encoded format back to the images.
-
-    #Suppose , from UI we are transferring data to the server and we don't want anyone to decode our dataset so in that case
-    # we can encode our dataset and then transfer.
-
-    #----------------------------------------- CONCEPT ---------------------------------------------------------------------
-
-    ## Images will be onverted into Base64 and this images which we have converted to base64 will be send to the model.
-    ## Base64 is a array kind of structure and whenever we try to transfer data from client side(UI) to server side(model.h5)
-    ## you are supposed to transfer those images in terms of base64 or any other encoding format.
+   
